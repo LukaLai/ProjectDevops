c4d27281ccf1fa2086d17b411875b1142e366fa8
Sebastopaul <sebastien.lamard.pro@gmail.com>
2024-04-17 10:25:22 +0200

    fix(snapshots): fixed snapshot tests
    some tests had errors
    
    #14

--- __tests__/snapshots/post-header-snapshot.js
+++ __tests__/snapshots/post-header-snapshot.js
@@ -2,6 +2,10 @@ import { render } from '@testing-library/react'
 import { PostHeader } from '../../src/app/_components/post-header'
 
 it('renders homepage unchanged', () => {
-    const { container } = render(<PostHeader date={'2024-04-17T05:35:07.322Z'} author={'jj'} title={'test'} coverImage={'/public/assets/blog/authors/jj.jpeg'}/>)
+    const author = {
+        name: 'jj',
+        picture: '/public/assets/blog/authors/jj.jpeg'
+    }
+    const { container } = render(<PostHeader date={'2024-04-17T05:35:07.322Z'} author={author} title={'test'} coverImage={'/public/assets/blog/authors/jj.jpeg'}/>)
     expect(container).toMatchSnapshot()
 })
